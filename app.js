57eab490101eb9ede7d0002e5d84b3baa57228d0
L C <LF9904@gmail.com>
2025-04-08 00:14:13 -0400

    p1 & p2 button

--- app.js
+++ app.js
@@ -5,4 +5,27 @@ const resetButton = document.querySelector("#resetButton");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
-let p1Score = 0;
+let p1Score = 0,
+  winningScore = 5,
+  p2Score = 0,
+  gameOver = false;
+
+p1Button.addEventListener("click", () => {
+  if (!gameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      gameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
+});
+
+p2Button.addEventListener("click", () => {
+  if (!gameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      gameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+});
