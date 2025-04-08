179dc0800453df60aa43859f91343f340fc81cd8
L C <LF9904@gmail.com>
2025-04-07 23:56:57 -0400

    added buttons and variables to the button

---
+++ app.js
@@ -0,0 +1,8 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const resetButton = document.querySelector("#resetButton");
+
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+
+let p1Score = 0;
